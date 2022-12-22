cac4e0566cc5c3a372bd9d7062f448dff9aa3998
Kabarthini67 <120014033+Kabarthini67@users.noreply.github.com>
2022-12-22 10:58:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import {  Password } from '@mui/icons-material';
+import './App.css';
+import Thiyaga from './components/Thiyaga';
+import Kab from './District/Kab';
+
+function App()
+{
+  return(
+    
+    <div classname="app">
+    <Password/>
+   
+    </div>
+  );
+}
+
+export default App
